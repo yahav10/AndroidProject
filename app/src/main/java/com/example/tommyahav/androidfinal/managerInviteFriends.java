3d13203fd74b7fc538e4692d203bdeeb01f09dbf
TomGoldberg <tomg3210@gmail.com>
2015-12-20 23:56:17 +0200

    new managerSignUpLayout java class import the class into the manifest (must!!)
    Tom - when you wanna ass a new activity, do this :
    File->New->Activity->...
    
    This will automatically adds :
    1. Layout
    2. Class file
    3. Additional Line in the Manifest

---
+++ app/src/main/java/com/example/tommyahav/androidfinal/managerInviteFriends.java
@@ -0,0 +1,109 @@
+package com.example.tommyahav.androidfinal;
+
+/**
+ * Created by TomGoldberg on 20.12.15.
+ */
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.util.Log;
+
+public class managerInviteFriends extends AppCompatActivity {
+
+    private static final String LOG = "managerInviteFriends";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_friends);
+        Log.i(LOG, "onCreate()");
+
+//        //logInButton event listender
+//        Button logInButton = (Button)findViewById(R.id.managerSignUpButton);
+//        logInButton.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                System.out.println("logInButton was clicked");
+//            }
+//        });
+//
+//        //signUpButton event listener
+//        Button managerSignUpButton = (Button)findViewById(R.id.managerSignUpButton);
+//        managerSignUpButton.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                System.out.println("managerSignUpButton was clicked");
+//
+//            }
+//        });
+    }
+
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(LOG , "onStart()");
+        System.out.println("onStart()");
+    }
+
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(LOG , "onRestart()");
+        System.out.println("onRestart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(LOG , "onResume()");
+        System.out.println("onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(LOG , "onPause()");
+        System.out.println("onPause()");
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(LOG , "onStop()");
+        System.out.println("onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(LOG , "onDestroy()");
+        System.out.println("onDestroy()");
+    }
+
+}
+
