3cfe8f3ae50eae8dc1f44fe5a460c17ea41fc9d3
TomGoldberg <tomg3210@gmail.com>
2015-12-20 16:02:42 +0200

    new managerSignUpLayout java class import the class into the manifest (must!!)
    Tom - when you wanna ass a new activity, do this :
    File->New->Activity->...
    
    This will automatically adds :
    1. Layout
    2. Class file
    3. Additional Line in the Manifest

--- app/src/main/java/com/example/tommyahav/androidfinal/MainActivity.java
+++ app/src/main/java/com/example/tommyahav/androidfinal/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.tommyahav.androidfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(LOG , "onCreate()");
+
+        //logInButton event listender
+        Button logInButton = (Button)findViewById(R.id.loginBottun);
+        logInButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("logInButton was clicked");
+            }
+        });
+
+        //signUpButton event listener
+        Button signUpButton = (Button)findViewById(R.id.signUp_button);
+        signUpButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("signUpButton was clicked");
+                Intent intent = new Intent( getApplicationContext() , managerSignUp.class );
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
+
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -34,4 +62,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(LOG , "onStart()");
+        System.out.println("onStart()");
+    }
+
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(LOG , "onRestart()");
+        System.out.println("onRestart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(LOG , "onResume()");
+        System.out.println("onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(LOG , "onPause()");
+        System.out.println("onPause()");
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(LOG , "onStop()");
+        System.out.println("onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(LOG , "onDestroy()");
+        System.out.println("onDestroy()");
+    }
+
 }
